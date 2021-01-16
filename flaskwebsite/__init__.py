6f2b295040b26b1deb302db8876d213a4998b214
Ovicron <40534384+Ovicron@users.noreply.github.com>
2021-01-15 19:15:26 -0500

    Update __init__.py

--- flaskwebsite/__init__.py
+++ flaskwebsite/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather_api.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS '] = False
 
 
